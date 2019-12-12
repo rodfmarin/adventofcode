0d4d9271edef348298289f5e7e31db5fd31734d6
rodfmarin <rodrigofmarin@gmail.com>
2019-12-11 18:09:43 -0800

    solved day 4.b

--- 2019/4/main.py
+++ 2019/4/main.py
@@ -51,6 +51,67 @@ def test_stack(stack, previous_digit=0):
             return False
 
 
+def make_number_map(number):
+    number_map = {}
+
+    for digit in str(number):
+        if number_map.get(digit):
+            number_map[digit] += 1
+        else:
+            number_map[digit] = 1
+    return number_map
+
+
+def double_digits_check(number):
+    """
+    For day 4.b, the additional constraint:
+    An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+    Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+    112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+    123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+    111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+    How many different passwords within the range given in your puzzle input meet all of the criteria?
+    :param number:
+    :return: True|False
+    """
+
+    # NB: How to approach?
+    #   do we just keep track of every digit seen in the number?
+    #   i.e.
+    #   |0|1|2|3|4|5|6|7|8|9|
+    #   |0|2|2|2|0|0|0|0|0|0|
+    #   If a number is seen more than once it is >1
+    #   if there is a number that is >1 but >2 then it does not meet the criteria; throw it out
+    #   if there are multiple numbers seen more than once the larger value set must only be = 2
+    #
+
+    number_map = make_number_map(number)
+    greater_than_one = {k: v for (k, v) in number_map.items() if v >= 2}
+
+    delete_list = []
+    for item in greater_than_one.items():
+        # how many digits do we have that appear more than once
+        if item[1] > 2:
+            delete_list.append(item[0])
+
+    for key in delete_list:
+        del greater_than_one[key]
+
+    if 0 < len(greater_than_one):
+        highest_seen = 0
+
+        for item in greater_than_one.items():
+            if item[1] > int(highest_seen):
+                highest_seen = item[0]
+
+        if greater_than_one[str(highest_seen[0])] == 2:
+            return True
+
+    return False
+
+
 def password_filter(number):
     """
     Using the rules, does a number pass through the filter
@@ -60,13 +121,11 @@ def password_filter(number):
 
     number_list = [int(d) for d in str(number)]
 
-    # having a list of numbers, starting at the beginning check for doubles
+    # having a list of numbers, starting at the beginning check for (valid) doubles
 
     double_digits = False
-    for index, digit in enumerate(number_list):
-        if index < len(number_list) - 1:
-            if number_list[index] == number_list[index+1]:
-                double_digits = True
+    if double_digits_check(number):
+        double_digits = True
 
     s = number_stack(number)
 
